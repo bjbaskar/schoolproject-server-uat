3664d584856d98b35b04d4b98240ffa0dc6f43ca
Baskaran Balaganesan <bjbaskar@yahoo.com>
2020-06-27 22:56:46 +0800

    Update _bootstrap.js

--- _migrations/_bootstrap.js
+++ _migrations/_bootstrap.js
@@ -46,7 +46,7 @@ class Bootstrap {
         typeorm_1.createConnection({
             type: "mysql",
             host: DB_HOST,
-            port: DB_PORT,
+           
             synchronize: DB_SYNCHRONIZE,
             logging: DB_LOGGING,
             dropSchema: DB_DROPSCHEMA,
